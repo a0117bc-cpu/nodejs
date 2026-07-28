962df9cb8d0eba4f7fcfbacea52ef2e75da4567c
a0117bc-cpu <a0117bc@gmail.com>
2026-07-28 05:22:06 +0000

    string changes

--- basics/string.js
+++ basics/string.js
@@ -1,7 +1,15 @@
 const name="shravani"
 const age =22
 
-console.log(`Hello my name is ${name} and my age is ${age}`)
+//console.log(`Hello my name is ${name} and my age is ${age}`)
 
-const myname = String('Shravanirw')
-const Myname1=new String('googlesr')
+const myname = new String('shravanirw')
+const myname1=new String('googlesr')
+
+
+console.log(myname[5])
+console.log(myname.big())
+console.log(myname.bold())
+console.log(myname.toUpperCase())
+console.log(myname.toLocaleLowerCase())
+console.log(myname.trim())
